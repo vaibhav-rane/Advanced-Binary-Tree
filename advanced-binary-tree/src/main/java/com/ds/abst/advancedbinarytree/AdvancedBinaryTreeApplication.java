57140c1e97431a71e4e75e1ad40d3f5b341b2a8b
varane <vaibhav.rane@copart.com>
2022-07-22 20:26:10 -0400

    Init Commit

---
+++ advanced-binary-tree/src/main/java/com/ds/abst/advancedbinarytree/AdvancedBinaryTreeApplication.java
@@ -0,0 +1,13 @@
+package com.ds.abst.advancedbinarytree;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdvancedBinaryTreeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdvancedBinaryTreeApplication.class, args);
+	}
+
+}
---
+++ advanced-binary-tree/src/test/java/com/ds/abst/advancedbinarytree/AdvancedBinaryTreeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ds.abst.advancedbinarytree;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdvancedBinaryTreeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
